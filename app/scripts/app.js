f3e6457e340333186f81a678274678090010a2d6
Clayton Minicus <cminicu1@jhu.edu>
2015-11-16 12:28:02 -0500

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hearthstoneApp
+ * @description
+ * # hearthstoneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hearthstoneApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
